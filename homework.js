15e539ecabddbab959acd94135ae0cf57c91399a
David Kirk <david.is.kirk@gmail.com>
2019-03-26 09:54:51 -0400

    did some maff

--- homework.js
+++ homework.js
@@ -1,26 +1,68 @@
 // 1. Set the variable `givenName` to the string "Addison".
-
+let givenName = "Addison"
 // 2. Set candies equal to 20, people to 6, and leftover equal
-// to the remainder of dividing 20 by 6.
+// to the remainder of dividing 20 by 6
+let candies = 20
+let people = 6 
+let leftover = 2
+
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+
+let name = 'Chase'
+function greeting(name){
+    
+    return `Hello, ${name}!`
+}
+
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
+function isOdd(x){
+   if ((x % 2) === 1 || (x % 2) === -1)
+      
+   return true
+   else
+   return false
+   
+
+
+        }
+
+
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+function isEven(x){
+    if ((x % 2) === 0 || (2 % x) === 0)
+    return true
+   else
+    return false
+
+
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
 
+function fahrenheitToCelsius(temperature) {
+    return (( temperature - 32) * (5/9))
+    
+}
+
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit(celsius){
+     return ((celsius * 9/5) + 32)
+
+
+}
+
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -29,9 +71,24 @@
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+
+function fahrenheitToKelvin (ftemp){
+    return fahrenheitToCelsius(ftemp) + 273.15
+
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+
+function lesser(x, y){
+    if(x > y)
+    return y;
+    else 
+    return x 
+
+
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -42,6 +99,21 @@
 // es - ¡Hola, <name>!
 // fr - Bonjour, <name>!
 // eo - Saluton, <name>!
+
+function multigreeting (name,code){
+    if (code === 'en')
+        return `Hello, + ${name}!`;
+    else if (code === 'es')
+        return `!Hola, + ${name}!`;
+    else if (code === 'fr')
+        return `Bonjour, + ${name}!`;
+    else if (code === 'eo' )
+        return `Saluton, + ${name}!`
+}
+  
+
+
+
 //
 // If any other language code is used, return nothing.
 
@@ -53,6 +125,9 @@
 
 // 12. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
+//var arr = [3,4,5,7,6,8]
+//return(arr.sort())
 
 // 13. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
